7704edd8aa6ef0bbd6a05a215b502ac717c1ed9b
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-09-06 17:14:35 +0300

    Params and searchParams routing added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,26 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  return <div className={styles.mapContainer}>MAP</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate('form');
+      }}
+    >
+      <h1>MAP</h1>
+      <h2>
+        Position {lat}, {lng}
+      </h2>
+    </div>
+  );
 }
 
 export default Map;
